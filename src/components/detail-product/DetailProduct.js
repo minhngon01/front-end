471d22b6364c83c225719a311b11ec8a535e043b
Nguyen Tan Dung <tandung251298@gmail.com>
2019-04-07 08:03:55 +0700

    implement detail product page - 80% done

--- src/components/detail-product/DetailProduct.js
+++ src/components/detail-product/DetailProduct.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import DetailInfoProduct from './detail-info-product/DetailInfoProduct'
 class DetailProduct extends React.Component{
   render(){
     return(
       <div>
+        <DetailInfoProduct/>
       </div>
     )
   }
