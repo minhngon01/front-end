e0ca9cb2ed7b1103a1604b9354694a0aee316752
Minh Nguyen <minhnt28101998@gmail.com>
2019-05-14 09:20:23 +0000

    Update StarRating.js

--- src/components/Fragment/StarRating/StarRating.js
+++ src/components/Fragment/StarRating/StarRating.js
@@ -2,12 +2,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
-class StarRating extends React.Component  {
+export default class StarRating extends React.Component  {
   constructor(props){
     super(props);
     this.state={
       star: "",
+      submitted: false,
       color1:"DarkSlateGrey",
       color2:"DarkSlateGrey",
       color3:"DarkSlateGrey",
@@ -30,37 +30,97 @@ class StarRating extends React.Component  {
     this.handleSubmit4=this.handleSubmit4.bind(this);
     this.handleSubmit5=this.handleSubmit5.bind(this);
   }
-  handleSubmit1() { 
-    this.setState({star : 1})
+  
+  handleSubmit1() {  
+    if(localStorage.getItem("checkLogin")==="false"){
+      window.location.replace('/signin')
+    }
+    else{
+    this.setState({
+      color1:'yellow',
+      submitted: true,
+      star : 1
+    })
+  }
+    
   }
   handleSubmit2() {
-    this.setState({star : 2})
+    if(localStorage.getItem("checkLogin")==="false"){
+      window.location.replace('/signin')
+    }
+    else{
+    this.setState({
+      color1:'yellow',
+      color2:'yellow',
+      submitted: true,
+      star : 2
+    })
+    }
   };
   handleSubmit3(){
-    this.setState({star : 3})
+    if(localStorage.getItem("checkLogin")==="false"){
+      window.location.replace('/signin')
+    }
+    else{
+      this.setState({
+        color1:'yellow',
+        color2:'yellow',
+        color3:'yellow',
+        submitted: true,
+        star : 3
+      })
+    }
   }
    handleSubmit4(){
-    this.setState({star : 4})
+    if(localStorage.getItem("checkLogin")==="false"){
+      window.location.replace('/signin')
+    }
+    else{
+      this.setState({
+        color1:'yellow',
+        color2:'yellow',
+        color3:'yellow',
+        color4:'yellow',
+        submitted: true,
+        star : 4
+      })
+    }
   }
   handleSubmit5(){
-    this.setState({star : 5})
+    if(localStorage.getItem("checkLogin")==="false"){
+      window.location.replace('/signin')
+    }
+    else{
+      this.setState({
+        color1:'yellow',
+        color2:'yellow',
+        color3:'yellow',
+        color4:'yellow',
+        color5:'yellow',
+        submitted: true,
+        star : 5
+      })
+    }
   }
   onHover1(){
     this.setState({
-      color1:'yellow'
+      color1:'yellow',
+      submitted: false
     })
   }
   onHover2(){
     this.setState({
       color1:'yellow',
-      color2:'yellow'
+      color2:'yellow',
+      submitted: false
     })
   }
   onHover3(){
     this.setState({
       color1:'yellow',
       color2:'yellow',
-      color3:'yellow'
+      color3:'yellow',
+      submitted: false
     })
   }
   onHover4(){
@@ -68,7 +128,8 @@ class StarRating extends React.Component  {
       color1:'yellow',
       color2:'yellow',
       color3:'yellow',
-      color4:'yellow'
+      color4:'yellow',
+      submitted: false
     })
   }
   onHover5(){
@@ -77,55 +138,71 @@ class StarRating extends React.Component  {
       color2:'yellow',
       color3:'yellow',
       color4:'yellow',
-      color5:'yellow'
+      color5:'yellow',
+      submitted: false
     })
   }
   onOut1(){
-    this.setState({
-      color:'DarkSlateGrey'
-    })
-  }
-  onOut2(){
+    if (!this.state.submitted){
     this.setState({
       color1:'DarkSlateGrey',
-      color2:'DarkSlateGrey'
+      submitted: false
     })
   }
-  onOut3(){
+  }
+  onOut2(){
+    if (!this.state.submitted){
     this.setState({
       color1:'DarkSlateGrey',
       color2:'DarkSlateGrey',
-      color3:'DarkSlateGrey'
+      submitted: false
     })
   }
-  onOut4(){
+  }
+  onOut3(){
+    if (!this.state.submitted){
     this.setState({
       color1:'DarkSlateGrey',
       color2:'DarkSlateGrey',
       color3:'DarkSlateGrey',
-      color4:'DarkSlateGrey'
+      submitted: false
     })
   }
-  onOut5(){
+  }
+  onOut4(){
+    if (!this.state.submitted){
     this.setState({
       color1:'DarkSlateGrey',
       color2:'DarkSlateGrey',
       color3:'DarkSlateGrey',
       color4:'DarkSlateGrey',
-      color5:'DarkSlateGrey'
+      submitted: false
     })
+  }
+  }
+  onOut5(){
+    if (!this.state.submitted){
+    this.setState({
+      color1:'DarkSlateGrey',
+      color2:'DarkSlateGrey',
+      color3:'DarkSlateGrey',
+      color4:'DarkSlateGrey',
+      color5:'DarkSlateGrey',
+      submitted: false
+    })
+  }
   }
   
   render() {
     return(
+    
     <div className="rating-product" >
-      <FontAwesomeIcon onClick={this.handleSubmit1} onMouseOver={this.onHover1} onMouseOut={this.onOut1} className="icon-star" icon="star" color={this.state.color1} />
-      <FontAwesomeIcon onClick={this.handleSubmit2} onMouseOver={this.onHover2} onMouseOut={this.onOut2} className="icon-star" icon="star" color={this.state.color2} />
-      <FontAwesomeIcon onClick={this.handleSubmit3} onMouseOver={this.onHover3} onMouseOut={this.onOut3} className="icon-star" icon="star" color={this.state.color3} />
-      <FontAwesomeIcon onClick={this.handleSubmit4} onMouseOver={this.onHover4} onMouseOut={this.onOut4} className="icon-star" icon="star" color={this.state.color4} />
-      <FontAwesomeIcon onClick={this.handleSubmit5} onMouseOver={this.onHover5} onMouseOut={this.onOut5} className="icon-star" icon="star" color={this.state.color5} />
+      <FontAwesomeIcon onClick={this.handleSubmit1} onMouseOver={this.onHover1}  onMouseOut={this.onOut1} className="icon-star" icon="star" color={this.state.color1} />
+      <FontAwesomeIcon onClick={this.handleSubmit2} onMouseOver={this.onHover2}  onMouseOut={this.onOut2} className="icon-star" icon="star" color={this.state.color2} />
+      <FontAwesomeIcon onClick={this.handleSubmit3} onMouseOver={this.onHover3}  onMouseOut={this.onOut3} className="icon-star" icon="star" color={this.state.color3} />
+      <FontAwesomeIcon onClick={this.handleSubmit4} onMouseOver={this.onHover4}  onMouseOut={this.onOut4} className="icon-star" icon="star" color={this.state.color4} />
+      <FontAwesomeIcon onClick={this.handleSubmit5} onMouseOver={this.onHover5}  onMouseOut={this.onOut5} className="icon-star" icon="star" color={this.state.color5} />
     </div>
     )
 }
 }
-export default StarRating;
