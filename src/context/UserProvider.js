ddb0e0163f6b827d748b2db6a133ce6b11617654
Minh Nguyen <minhnt28101998@gmail.com>
2019-05-19 06:22:46 +0000

    Update UserProvider.js

--- src/context/UserProvider.js
+++ src/context/UserProvider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import jwt from 'jsonwebtoken';
+import axios from 'axios';
 export const UserContext = React.createContext();
 
 export default class UserProvider extends React.Component {
@@ -10,53 +11,114 @@ export default class UserProvider extends React.Component {
     localStorage.setItem("checkLogin",false);
     this.state={
       isLogin : false,
+      user_id        : "",
       user_firstname : "",
       user_lastname  : "",
       user_phone     : "",
       user_email     : "",
       user_address   : "",
       is_female      : "",
-      order: [],
+      token          : "",
+      carts: [],
       dispatchLogin : (JsonWebToken) => {
         var decoded = jwt.decode(JsonWebToken);
-        var {user_email, user_phone, is_female, user_firstname, user_lastname, user_address} = decoded;
+        var {user_id,user_email, user_phone, is_female, user_firstname, user_lastname, user_address} = decoded;
         this.setState({
           isLogin : true,
+          user_id        : user_id,
           user_firstname : user_firstname,
           user_lastname  : user_lastname,
           user_phone     : user_phone,
           user_email     : user_email,
           user_address   : user_address,
           is_female      : is_female,
-
+          token          : JsonWebToken,
           /*
             set order here
           */
         })
+        localStorage.setItem('user_id',this.state.user_id);
         var x = localStorage.getItem("checkLogin");
         localStorage.setItem("checkLogin",true);
+        this.state.getListCart(user_id,JsonWebToken);
         localStorage.setItem("token", JsonWebToken);
+        localStorage.setItem("isLogin", true);
+
       },
       dispatchLogout : () => {
         this.setState({
           isLogin : false,
+          user_id        : "",
           user_firstname : "",
           user_lastname  : "",
           user_phone     : "",
           user_email     : "",
           user_address   : "",
           is_female      : "",
-          order: [],
+          token          : "",
+          carts: [],
         })
         var x = localStorage.getItem("checkLogin");
+        var y = localStorage.getItem("user_id");
         localStorage.setItem("checkLogin",false);
         localStorage.removeItem("token");
+        localStorage.removeItem("isLogin");
+
       },
-      //
-      //
-      // addToCart = ( product_id ) => {
-      //
-      // },
+      getListCart : (user_id,token) => {
+        console.log("@@@@@@@")
+        axios({
+          method : "get",
+          url    : "http://localhost:3003/carts/",
+          params   : {
+            user_id : user_id,
+          },
+          headers: {'Authorization': "Bearer " + token}
+        })
+        .then( res => {
+          console.log(res);
+          const {data} = res
+          data.success && this.setState({carts : data.carts})
+        })
+      },
+      addToCart : ( product_id ) => _ =>{
+        console.log("QQQ")
+        axios({
+          method : "post",
+          url    : "http://localhost:3003/carts/",
+          data   : {
+            product_id : product_id,
+            user_id : this.state.user_id,
+          },
+          headers: {'Authorization': "Bearer " + this.state.token}
+        })
+        .then( res => {
+          console.log(res);
+          const {data} = res
+          data.success && this.setState({carts : [...this.state.carts,data.cart_product]})
+        })
+        // this.setState({order : [...this.state.order,product]})
+      },
+      deleteFromCart : (product_id) => _ => {
+        axios({
+          method : "delete",
+          url    : "http://localhost:3003/carts/",
+          data   : {
+            product_id : product_id,
+            user_id : this.state.user_id,
+          },
+          headers: {'Authorization': "Bearer " + this.state.token}
+
+        })
+        .then( res => {
+          const {data} = res
+          console.log("@@",data)
+          data.success && this.setState({carts : data.carts})
+        })
+      },
+      addToOrder : (product_id) => _ => {
+
+      }
       //
       // addToWishList = (product_id) => {
       //
@@ -66,9 +128,7 @@ export default class UserProvider extends React.Component {
       //
       // },
       //
-      // deleteFromCart = (product_id) => {
-      //
-      // },
+
       //
       // processPayment = () => {
       //
@@ -79,31 +139,34 @@ export default class UserProvider extends React.Component {
   componentDidMount(){
     if(localStorage.token){
       var decoded = jwt.decode(localStorage.token);
-      var {user_email, user_phone, is_female, user_firstname, user_lastname, user_address} = decoded;
+      var {user_id,user_email, user_phone, is_female, user_firstname, user_lastname, user_address} = decoded;
       this.setState({
         isLogin : true,
+        user_id        : user_id,
         user_firstname : user_firstname,
         user_lastname  : user_lastname,
         user_phone     : user_phone,
         user_email     : user_email,
         user_address   : user_address,
         is_female      : is_female,
+        token          : localStorage.token,
       })
+      this.state.getListCart(user_id,localStorage.token);
     }
   }
-  
+
 
   render(){
-    
+
     return (
-      <div>    
-      <UserContext.Provider value = {this.state} >      
-        {this.props.children}  
+      <div>
+      <UserContext.Provider value = {this.state} >
+        {this.props.children}
       </UserContext.Provider>
-      
+
       </div>
-     
-      
+
+
     )
   }
 }
