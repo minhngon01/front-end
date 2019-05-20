0cd9893222d4fba684f925b7ea0d8c76ed7dc440
Minh Nguyen <minhnt28101998@gmail.com>
2019-05-20 14:06:13 +0000

    Update Search.js

--- src/components/layout/header/search/Search.js
+++ src/components/layout/header/search/Search.js
@@ -4,14 +4,39 @@ import {Link} from 'react-router-dom';
 import CartPanel from '../../../Fragment/Cart/CartPanel';
 import MenuNav from '../menu-nav/MenuNav';
 import {UserContext} from '../../../../context/UserProvider';
+import axios from 'axios';
+import { Route, Redirect } from 'react-router';
 
 class Search extends Component{
   constructor(props){
     super(props);
     this.state={
-      toggle : false,
+      products: []
     }
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onSubmit(event){
+    event.preventDefault();
+    var query = this.input.value;
+    
+    this.componentDidMount(query);
+
+  }
+  
+  componentDidMount(query){
+    var api = 'http://localhost:3003/search/allcategory?product_name='
+    axios.post('http://localhost:3003/search/allcategory',{
+      product_name: query
+    })
+      .then(res => {
+    
+    this.setState({products : res.data});
+    console.log(this.state.products);
+  })
+      .catch(error => console.log(error));
+  }
+
   render(){
     return(
       <React.Fragment>
@@ -19,24 +44,38 @@ class Search extends Component{
           <Link to="/"><img className="c-navBar__middle-bar__logo"  src="https://devicer.cmsmasters.net/wp-content/uploads/2018/03/device-home-1-logo-retina.png" alt="Card image cap"/></Link>
           <div className="c-navBar__middle-bar__right">
             <div className="c-search-bar c-navBar__middle-bar__search">
-                <div className="dropdown">
-                  <button className="c-search-bar__dropdown-button dropdown-toggle  " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    All categories
-                  </button>
-                  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </div>
-                <input type="text" className="c-search-bar__input" placeholder="Search for product" />
+                 
+            <div class="selectdiv">
+              <label>
+                  <select>
+                      <option selected> All categories </option>
+                      <option>Camera</option>
+                      <option>Computer</option>
+                      <option>Headphone</option>
+                      <option>Smart Phone</option>
+                      <option>TV</option>
+                  </select>
+              </label>
+            </div>
+                <form onSubmit = {this.onSubmit}>
+                <input type="text" className="c-search-bar__input" placeholder="Search for products" name="product_name"
+                ref = {input => this.input = input} 
+                 />
+                 </form>
+
+                {this.state.products.length > 0 &&
+                  <Redirect to={{
+                    pathname: '/shop',
+                    state: { products: this.state.products }
+                  }}/>
+                }
             </div>
             <UserContext.Consumer>
               { state =>
             <div className="c-cart c-navBar__middle-bar__cart">
 
                   <div className="c-cart__icon">
-                    <div class="c-cart__number-products">{state.carts.length}</div>
+                    <div class="c-cart__number-productss">{state.carts.length}</div>
                     <FontAwesomeIcon className="icon-cart" icon="cart-plus" size="3x" color="DarkSlateGrey"/>
                   </div>
                   <div className="c-cart__your-cart">
